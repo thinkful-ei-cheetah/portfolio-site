6dddaa33603406f01520987eae62f57f79e312b6
Michael Verdi <michael.v.verdi@gmail.com>
2019-04-13 13:01:51 -0400

    updated typwriter to save html js side, so you don't see if briefly before page load

--- Typewriter.js
+++ Typewriter.js
@@ -1,82 +1,18 @@
 'use strict';
 
-// const TypeWriter = (function() {
-//   let typer = document.getElementById('typewriter');
-
-//   class TypeWriter {
-//     constructor() {
-//       this.HTML = typer.innerHTML;
-//       this.cursorPosition = 0;
-//       this.tag = '';
-//       this.writingTag = false;
-//       this.tagOpen = false;
-//       this.typeSpeed = 100;
-//       this.tempTypeSpeed = 0;
-//     }
-
-//     type() {
-//       typer.innerHTML = '';
-
-//       if (this.writingTag === true) {
-//         this.tag += this.HTML[this.cursorPosition];
-//       }
-
-//       if (this.HTML[this.cursorPosition] === '<') {
-//         this.tempTypeSpeed = 0;
-//         if (this.tagOpen) {
-//           this.tagOpen = false;
-//           this.writingTag = true;
-//         } else {
-//           this.tag = '';
-//           this.tagOpen = true;
-//           this.writingTag = true;
-//           this.tag += this.HTML[this.cursorPosition];
-//         }
-//       }
-  
-//       if (!this.writingTag && this.tagOpen) {
-//         this.tag.innerHTML += this.HTML[this.cursorPosition];
-//       }
-  
-//       if (!this.writingTag && !this.tagOpen) {
-//         if (this.HTML[this.cursorPosition] === ' ') {
-//           this.tempTypeSpeed = 0;
-//         }
-//         else {
-//           this.tempTypeSpeed = (Math.random() * this.typeSpeed) + 50;
-//         }
-//         typer.innerHTML += this.HTML[this.cursorPosition];
-//       }
-  
-//       if (this.writingTag === true && this.HTML[this.cursorPosition] === '>') {
-//         this.tempTypeSpeed = (Math.random() * this.typeSpeed) + 50;
-//         this.writingTag = false;
-//         if (this.tagOpen) {
-//           let newSpan = document.createElement('span');
-//           typer.appendChild(newSpan);
-//           newSpan.innerHTML = this.tag;
-//           this.tag = newSpan.firstChild;
-//         }
-//       }
-  
-//       this.cursorPosition += 1;
-//       if (this.cursorPosition < this.HTML.length - 1) {
-//         let that = this;
-//         setTimeout(that.type, that.tempTypeSpeed);
-//       }
-//     }
-//   }
-
-//   return TypeWriter;
-
-// }());
-
-
 const TypeWriter = (function() {
   let typer = document.getElementById('typewriter');
-  let HTML = typer.innerHTML;
-
-  typer.innerHTML = '';
+  let HTML = `
+    <span class="vh">const</span> buildDev = {
+      name: <span class="sh">'Michael Verdi'</span>,
+      type: <span class="sh">'Full Stack Developer'</span>,
+      location: <span class="sh">'Tampa'</span>,
+      skills:[<span class="sh">'React'</span>,
+              <span class="sh">'Node'</span>,
+              <span class="sh">'Rails'</span>,
+              <span class="sh">'Postgres'</span>,
+    };
+  `;
 
   let cursorPosition = 0,
     tag = '',

976df08a9a88e714fcae91e80c67ba6a04f86d74
Michael Verdi <michael.v.verdi@gmail.com>
2019-04-13 13:34:28 -0400

    mobile styling updates and minified images

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ $(function(){
   TypeWriter.type();
 
   $('#ice-breaker li').hover(function() {
-    $(this).find('p').removeClass('hidden');
+    $( this ).find('p').slideToggle();
   }, function() {
-    $(this).find('p').addClass('hidden');
+    $( this ).find('p').slideToggle();
   });
 });

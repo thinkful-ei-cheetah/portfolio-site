85758f4b13928b634c0244a89a1b9c2a542eedbe
Michael Verdi <michael.v.verdi@gmail.com>
2019-07-27 12:13:36 -0400

    add onClick for mobile when doing truth or lie, organized favicons, add styling for 320px for intro

--- index.js
+++ index.js
@@ -5,9 +5,19 @@
 $(function(){
   TypeWriter.type();
 
-  $('#ice-breaker li').hover(function() {
-    $( this ).find('p').slideToggle();
-  }, function() {
-    $( this ).find('p').slideToggle();
-  });
+  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    $('#ice-breaker li').on('click', () => {
+      $( this ).find('p').slideToggle();
+    }, function() {
+      $( this ).find('p').slideToggle();
+    });  
+  } else {
+    $('#ice-breaker li').hover(function() {
+      $( this ).find('p').slideToggle();
+    }, function() {
+      $( this ).find('p').slideToggle();
+    });
+  }
+
+  
 });
